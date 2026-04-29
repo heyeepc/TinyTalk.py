d2afa0c6d8ece7c9daf925b0636295f5d97c0a3d
heyeepc <2675458141@qq.com>
2026-04-29 22:29:59 +0800

    更新

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ class serverUI():
     global serverSocket
     flag = False
 
+def __init__(self):
+    self.root = tkinter.Tk()
+    self.root.title('python在线聊天-服务端')
+    self.frame = [tkinter.Frame(),tkinter.Frame(),tkinter.Frame(),tkinter.Frame()]
+    self.chatTextScrollBar = tkinter.Scrollbar(self.frame[0])
+    self.chatTextScrollBar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    ft=tkFont.Font(family='Fixdsys',size=11)
+    self.chatText['yscrollcommand'] = self.chatTextScrollBar.set
+    self.chatText.Pack(expand=1,fill=tkinter.BOTH)
