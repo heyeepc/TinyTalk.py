50f98e7ef0e551296855f0e006313826a87bc966
heyeepc <2675458141@qq.com>
2026-05-06 13:30:12 +0800

    更新

--- main.py
+++ main.py
@@ -20,4 +20,18 @@ def __init__(self):
     self.chatText['yscrollcommand'] = self.chatTextScrollBar.set
     self.chatText.Pack(expand=1,fill=tkinter.BOTH)
     label = tkinter.Label(self.frame[1],height=2)
+    label.pack(fill=tkinter.BOTH)
+    self.frame[1].pack(expand=1,fill=tkinter.BOTH)
+
+    self.inputTexScrollBar = tkinter.Scrollbar(self.frame[2])
+    self.inputTexScrollBar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+    ft=tkFont.Font(family='Fixdsys',size=11)
+    self.inputTex = tkinter.Text(self.frame[2],width=70,height=8,font=ft)
+    self.inputTex['yscrollcommand'] = self.inputTexScrollBar.set
+    self.inputTex.Pack(expand=1,fill=tkinter.BOTH)
+    self.inputTexScrollBar['command'] = self.inputTex.yview()
+    self.frame[2].pack(expand=1,fill=tkinter.BOTH)
+
+    
 
