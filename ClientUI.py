f00a73ec2ce5a319416825d7815d54020ff63c18
heyeepc <2675458141@qq.com>
2026-05-13 19:20:25 +0800

    更新

--- ClientUI.py
+++ ClientUI.py
@@ -3,3 +3,16 @@ import tkinter.font as tkFont
 import socket
 import threading
 import time
+
+class ClientUI():
+    local = '172.0.0.1'
+    port = 5505
+    global clientSock;
+    flag = False
+
+def __init__(self):
+    self.root = tkinter.Tk()
+    self.root.title("python在线聊天-客户端")
+
+    self.frame = [tkinter.Frame(),tkinter.Frame(),tkinter.Frame(),tkinter.Frame()]
+    
