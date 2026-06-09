ee565054bd843bab145aa760a4ab4d23d1acb418
heyeepc <2675458141@qq.com>
2026-06-09 23:36:51 +0800

    更新

--- ClientUI.py
+++ ClientUI.py
@@ -20,4 +20,19 @@ def __init__(self):
 
     ft = tkinter.font(family='fixdsys',size = 11)
     self.chatText=tkinter.Listbox(self.frame[0],width=70,height=18,font=ft)
+    self.chatText['yscrollcommand'] = self.chatTextscrollbar.set
+    self.chatText.pack(expand=1,fill=tkinter.BOTH)
+    self.chatTextscrollbar['command'] = self.chatText.yview()
+    self.frame[1].pack(expand=1,fill=tkinter.BOTH)\
+
+    self.inputTextScrollbar = tkinter.Scrollbar(self.frame[2])
+    self.inputTextScrollbar.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+
+    ft = tkinter.font(family='fixdsys',size = 11)
+    self.inputText = tkinter.Entry(self.frame[2],width=70 ,height=8,font=ft)
+    self.inputText['yscrollcommand'] = self.inputTextScrollbar.set
+    self.inputText.pack(expand=1,fill=tkinter.BOTH)
+    self.inputTextScrollbar['command'] = self.inputText.yview()
+    self.frame[2].pack(expand=1,fill=tkinter.BOTH)
+
     
