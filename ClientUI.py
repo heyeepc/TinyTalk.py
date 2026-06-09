a14f033f0d9a6e702ea6b1afebc0df1f7ddd7290
heyeepc <2675458141@qq.com>
2026-06-09 23:20:05 +0800

    更新

--- ClientUI.py
+++ ClientUI.py
@@ -15,4 +15,9 @@ def __init__(self):
     self.root.title("python在线聊天-客户端")
 
     self.frame = [tkinter.Frame(),tkinter.Frame(),tkinter.Frame(),tkinter.Frame()]
+    self.chatTextscrollbar = tkinter.Scrollbar(self.frame[0])
+    self.chatTextscrollbar.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+
+    ft = tkinter.font(family='fixdsys',size = 11)
+    self.chatText=tkinter.Listbox(self.frame[0],width=70,height=18,font=ft)
     
